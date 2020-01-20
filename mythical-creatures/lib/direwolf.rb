f399e779ce40acb7936e6f5f45732869bc12d150
Stephanie Friend <stephaniefriend91@gmail.com>
2020-01-20 15:03:26 -0700

    Complete Direwolf

---
+++ mythical-creatures/lib/direwolf.rb
@@ -0,0 +1,34 @@
+class Direwolf
+
+  attr_reader :name,
+              :home,
+              :size,
+              :starks_to_protect
+
+  def initialize(name, home = "Beyond the Wall", size = "Massive")
+    @name = name
+    @home = home
+    @size = size
+    @starks_to_protect = []
+  end
+
+  def protects(stark)
+    if (@home == stark.location) && (@starks_to_protect.length < 2)
+      @starks_to_protect << stark
+      stark.safe = true
+    end
+  end
+
+  def hunts_white_walkers?
+    return !(@starks_to_protect.length > 0)
+  end
+
+  def leaves(stark)
+    if !@starks_to_protect.include?(stark)
+      return stark
+    else
+      @starks_to_protect.delete(stark)
+      stark.safe = false
+    end
+  end
+end
---
+++ mythical-creatures/lib/stark.rb
@@ -0,0 +1,24 @@
+class Stark
+
+  attr_accessor :safe
+  attr_reader :name,
+              :location
+
+  def initialize(name, location = "Winterfell")
+    @name = name
+    @location = location
+    @safe = false
+  end
+
+  def safe?
+    @safe
+  end
+
+  def house_words
+    if !safe?()
+      return "Winter is Coming"
+    else
+      return "The North Remembers"
+    end
+  end
+end
