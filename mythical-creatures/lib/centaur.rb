9f4e9aa704eac621a4147f55cdf2815350d0785a
Stephanie Friend <stephaniefriend91@gmail.com>
2020-01-20 12:50:28 -0700

    Complete centaur

---
+++ mythical-creatures/lib/centaur.rb
@@ -0,0 +1,74 @@
+class Centaur
+#class constant 
+  MAX_ACTIONS_BEFORE_CRANKY = 3
+#identify external variables
+  attr_reader :name,
+              :breed,
+              :laying
+  @action #identify internal variables
+
+#assign all variables
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+    @action_count = 0
+    @laying = false
+  end
+
+  def shoot
+    if !cranky?() && !@laying
+      @action_count += 1
+      return "Twang!!!"
+    else
+      return "NO!"
+    end
+  end
+
+  def run
+    if !cranky?() && !@laying
+      @action_count += 1
+      return "Clop clop clop clop!!!"
+    else
+      return "NO!"
+    end
+  end
+
+  def cranky?
+    return @action_count >= MAX_ACTIONS_BEFORE_CRANKY
+  end
+
+  def sleep
+    if !@laying
+      return "NO!"
+    else
+      @action_count = 0
+    end
+  end
+
+  def laying?
+    return @laying
+  end
+
+  def standing?
+    return !@laying
+  end
+
+  def lay_down
+    @laying = true
+  end
+
+  def stand_up
+    @laying = false
+  end
+
+  def drink
+    if standing?()
+      if cranky?()
+        @action_count = 0
+        return
+      else
+        return "Stand back! I'm going to be sick!"
+      end
+    end
+  end
+end
